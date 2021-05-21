184fcdc6d506f1d7b11f05526ca91b600ce52a8f
samadhan563 <samadhan563@gmail.com>
2021-05-21 15:46:02 +0530

    Python program  added for type casting and user input

---
+++ Chapter_2/05_UserInput.py
@@ -0,0 +1,46 @@
+# -------------------------------------------------------------------------------------------------------
+#      Heading
+# Python program for Type casting
+'''
+    Auther:Samadhan Nana Gaikwad.
+    Location:Pune.
+    Company Name: Nuvolo solution pvt. ltd., Pune.
+'''
+# -------------------------------------------------------------------------------------------------------
+#       Program
+name=input("Enter Name :  \n")
+mobNum=int(input("Enter Mobile number \n")) # type is String
+print("name is ",name,"\nMobile number is ",mobNum)
+
+#input Operation 
+num1=int(input("Enter num1 : \n"))
+num2=int(input("Enter num2 : \n"))
+print("Result = ",num2+num1)
+print("Avg = ",(num2+num1)/2)
+
+#--------------------------------------------------------------------------------------------------------
+#       Output
+'''
+    D:\E-DAC Data\Python\Chapter_2>05_UserInput.py
+    Enter Name :
+    Samadhan Gaikwad
+    Enter Mobile number
+    9999999999
+    name is  Samadhan Gaikwad
+    Mobile number is  9999999999
+    Enter num1 :
+    20
+    Enter num2 :
+    50
+    Result =  70
+    Avg =  35.0
+'''
+# -------------------------------------------------------------------------------------------------------
+#       Notes
+'''   
+    input()
+        -- This function allows the user to take input from user keybard as a string
+        --Accept input as a string from user if user input is number but it accept as a string.
+'''
+# End
+# -------------------------------------------------------------------------------------------------------
