c81bda967555e8ece3af2210377d382ebf124602
samadhan563 <samadhan563@gmail.com>
2021-05-21 12:15:28 +0530

    hello python

---
+++ 01_hello.py
@@ -0,0 +1,4 @@
+import os
+print("Hello world") 
+#module---> Module is a file containing code writtern by some body else usually which can be imported and used our program 
+#pip---> pip is the package manager for python you can use pip to inbstall a module on u r system
