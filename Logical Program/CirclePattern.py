c315d97326031991ade7f6f6f08c1408c7c17914
samadhan563 <samadhan563@gmail.com>
2021-05-21 22:01:52 +0530

    simple change only file rename

---
+++ Logical Program/CirclePattern.py
@@ -0,0 +1,22 @@
+# Program for circuler pattern
+'''
+    Auther : Samadhan Gaikwad.
+    Software Devloper
+    Location: Pune.
+'''
+
+import turtle
+# s = turtle.getscreen()
+t = turtle.Turtle()
+turtle.bgcolor("black")
+t.speed(5)
+t.goto(-300, 0)
+for i in range(5):
+    for j in ["magenta", "red", "blue", "green", "cyan", "yellow", "white", "purple"]:
+        t.color(j)
+        t.pensize(3)
+        t.circle(150)
+        t.forward(20)
+        t.speed(11)
+t.hideturtle()
+turtle.done()
---
+++ Logical Program/Generate_Random.py
@@ -0,0 +1,15 @@
+# Program for generate random number
+'''
+    Auther : Samadhan Gaikwad.
+    Software Devloper
+    Location: Pune.
+'''
+
+import random
+while True:
+    num=random.randint(0,51)
+    if num==50:
+        break
+    else :
+        print(num)
+print("Bye !!!!")
