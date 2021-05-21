d48323114ea0ccbf058f063fbff8326a947c284c
samadhan563 <samadhan563@gmail.com>
2021-05-21 13:39:11 +0530

    Program of playsound module

---
+++ 04_Playsound_Module.py
@@ -0,0 +1,8 @@
+#Program for play sound module
+'''
+    Auther:--- Samadhan Nana Gaikwad
+    Company Name:--- Nuvolo solution pvt. ltd., Pune.
+'''
+
+from playsound import playsound
+playsound("C:\\Users\samad\\Downloads\\Khamb - Amber Vashisht Ringtone.mp3")
