5d3d3fa1f6c2a90d4208069f3660ecc44a5ad3dd
samadhan563 <samadhan563@gmail.com>
2021-05-21 13:00:50 +0530

    Program for table of 5

---
+++ 03_Table_Of_5.py
@@ -0,0 +1,9 @@
+#Program of print  the table of 5 ..
+'''
+    Auther:--- Samadhan Nana Gaikwad
+    Company Name:--- Nuvolo solution pvt. ltd., Pune.
+'''
+t=5
+n=10
+for i in range(0,n) :
+    print("t*",i+1,"=",((i+1)*t))
