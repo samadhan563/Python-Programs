1afbf226932cee289967d63fd26b652ec8b510bd
samadhan563 <samadhan563@gmail.com>
2021-05-21 12:20:16 +0530

    hello python

--- 01_hello.py
+++ 01_hello.py
@@ -1,4 +1,18 @@
 import os
 print("Hello world") 
-#module---> Module is a file containing code writtern by some body else usually which can be imported and used our program 
-#pip---> pip is the package manager for python you can use pip to inbstall a module on u r system
+
+
+
+#module---> Module is a file containing code writtern by some body else usually which can be imported and used our programs
+#
+
+#pip---> pip is the package manager for python you can use pip to inbstall a module on u r systemecho "
+
+#*****************Gi Command*************# 
+# # Python-Programs" >> README.md
+#git init
+#git add README.md
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin https://github.com/samadhan563/Python-Programs.git
+#git push -u origin main
