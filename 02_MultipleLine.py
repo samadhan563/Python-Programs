ac647f6851f1f1c0960e4dc0ef71d254c921ea6c
samadhan563 <samadhan563@gmail.com>
2021-05-21 12:37:47 +0530

    Second code string from multiple line....

---
+++ 02_MultipleLine.py
@@ -0,0 +1,17 @@
+#Program of print string with more than one line..
+'''
+    Auther:--- Samadhan Nana Gaikwad
+    Company Name:--- Nuvolo solution pvt. ltd., Pune.
+'''
+print('''Twinkle, twinkle, little star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle, twinkle little star
+How I wonder what you are
+When the blazing sun is gone
+When he nothing shines upon
+Then you show your little light
+Twinkle, twinkle, all the night
+Twinkle, twinkle, little star
+How I wonder what you are.''')
