fcde970fc90a2b6be47ed3ae83a51fea49b16ca1
samadhan563 <samadhan563@gmail.com>
2021-05-22 10:39:25 +0530

    List code are modified.....

--- Chapter_04/04_DisplayPrimeNumberFromList.py
+++ Chapter_04/04_DisplayPrimeNumberFromList.py
@@ -6,21 +6,23 @@
 '''
 #create list 
 arr=[10,21,5,1,2,3,4,5,6,7,8,9]
-print("Serise of 1 to 100 prime number : ")
+print("Prime number from list : ")
 for i in range(0,len(arr)):
     count=0
     for j in range(1,arr[i]+1):
         if ((arr[i]%j)==0):
             count+=1
     if(count==2):
-        print(arr[i])
+        print(arr[i],end=" ")
 
 
 
 #---------------------------------------------------------------------------------------------
 #       Output
 '''
-
+    D:\E-DAC Data\Python\Chapter_04>python 04_DisplayPrimeNumberFromList.py
+    Prime number from list :
+    5 2 3 5 7
 '''
 #---------------------------------------------------------------------------------------------
 
