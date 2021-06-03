f4f1c5d174249d62cde360d53bf57436ec24e720
samadhan563 <samadhan563@gmail.com>
2021-06-03 22:24:50 +0530

    programs of if-else ..

---
+++ Chapter_05/06_IfElse.py
@@ -0,0 +1,22 @@
+# Program for Set methods in python
+'''
+    Author : Samadhan Gaikwad.
+    Software Developer
+    Location: Pune.
+'''
+names = ["Samadhan", "Rohit", "Arjun", "Sami", "Sayali", "Vinod"]
+name=input("Enter name \n")
+if name in names:
+    print(name," present in list..\n")
+else:
+    print(name," not present in list..")
+
+# ---------------------------------------------------------------------------------------------
+#       Output
+'''
+D:\E-DAC Data\Python\Chapter_05>python 06_IfElse.py
+Enter name
+Samadhan
+Samadhan  present in list..
+'''
+# ---------------------------------------------------------------------------------------------
