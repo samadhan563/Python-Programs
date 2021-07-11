8d7318a72fa8dbe64a31c593421bdbf0522cbaf7
samadhan563 <samadhan563@gmail.com>
2021-07-11 17:10:06 +0530

    OOP's Concept updated....

--- Chapter_07/01_Employee.py
+++ Chapter_07/01_Employee.py
@@ -1,14 +1,23 @@
-import Employee
+from Employee import Employee
+# Single object of employee
+# id = int(input())
+# firstName = str(input())
+# lastName = str(input())
+# salary = float(input())
+# emp= Employee(id, firstName, lastName, salary)
+# print(emp.toString())
 
+# array of object of employee
 emp = []
 count = int(input("Enter List size : "))
 for i in range(1, count+1):
     print("Enter details like : First Name, Last name, Salary")
-    id=int(i)
-    firstName=str(input())
-    lastName=str(input())
-    salary=float(input())
-    emp[i]=Employee(id,firstName,lastName, salary)
+    id = int(i)
+    firstName = str(input())
+    lastName = str(input())
+    salary = float(input())
+    emp.append(Employee(id, firstName, lastName, salary))
+    # emp[i]=Employee(id, firstName, lastName, salary)
+    i += 1
 for e in emp:
     print(e.toString())
-    
--- Chapter_07/Employee.py
+++ Chapter_07/Employee.py
@@ -27,18 +27,16 @@ class Employee:
 # salary = float(input())
 # emp= Employee(id, firstName, lastName, salary)
 # print(emp.toString())
-emp = []
-count = int(input("Enter List size : "))
-for i in range(1, count+1):
-    print("Enter details like : First Name, Last name, Salary")
-    id = int(i)
-    firstName = str(input())
-    lastName = str(input())
-    salary = float(input())
-    emp.append(Employee(id, firstName, lastName, salary))
-    # emp[i]=Employee(id, firstName, lastName, salary)
-    i += 1
+# emp = []
+# count = int(input("Enter List size : "))
+# for i in range(1, count+1):
+#     print("Enter details like : First Name, Last name, Salary")
+#     id = int(i)
+#     firstName = str(input())
+#     lastName = str(input())
+#     salary = float(input())
+#     emp.append(Employee(id, firstName, lastName, salary))
+#     i += 1
 
-for e in emp:
-    print(e.toString())
-    # print(e)
+# for e in emp:
+#     print(e.toString())
