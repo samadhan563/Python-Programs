33f45560deaba4a7f958fa0fce3121e92c2d8249
samadhan563 <samadhan563@gmail.com>
2021-07-11 17:27:54 +0530

    OOP's Concept updated....

--- Chapter_07/01_Employee.py
+++ Chapter_07/01_Employee.py
@@ -1,3 +1,13 @@
+# Program for if else in python
+'''
+    Author : Samadhan Gaikwad.
+    Software Developer
+    Location: Pune.
+'''
+
+# ---------------------------------------------------------------------------------------------
+#  Program
+
 from Employee import Employee
 # Single object of employee
 # id = int(input())
@@ -21,3 +31,26 @@ for i in range(1, count+1):
     i += 1
 for e in emp:
     print(e.toString())
+
+# ---------------------------------------------------------------------------------------------
+#  Output
+'''
+    PS D:\E-DAC Data\Python> & C:/Users/samad/AppData/Local/Programs/Python/Python39/python.exe "d:/E-DAC Data/Python/Chapter_07/01_Employee.py"
+    Enter List size : 2
+    Enter details like : First Name, Last name, Salary
+    aaaaa
+    aaaaa
+    22222
+    Enter details like : First Name, Last name, Salary
+    sssss
+    sssss
+    33333 
+    Emp Id : 1
+    Emp First Name : aaaaa
+    Emp Last Name         : aaaaa
+    Emp Salary : 22222.0
+    Emp Id : 2
+    Emp First Name : sssss
+    Emp Last Name         : sssss
+    Emp Salary : 33333.0
+'''
